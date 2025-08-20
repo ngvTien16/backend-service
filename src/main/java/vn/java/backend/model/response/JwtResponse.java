7e29dc567f1524e8fa6193c4b43854793296ac5c
ngvTien16 <nguyenvantien2004.vinhphuc@gmail.com>
2025-08-20 22:44:48 +0700

    init project

---
+++ src/main/java/vn/java/backend/model/response/JwtResponse.java
@@ -0,0 +1,10 @@
+package vn.java.backend.model.response;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Data
+@AllArgsConstructor
+public class JwtResponse {
+    private String token;
+}
